a804a4265688d6b51741e572c3d44328daf8f9b1
ABHISHEK-RAI007 <abraiskt@gmail.com>
2022-05-26 08:47:19 +0530

    JS Files

---
+++ 01.js
@@ -0,0 +1,8 @@
+"use strict";
+// console.log can print something on console
+
+console.log("Hello World");
+
+
+
+
---
+++ 02.js
@@ -0,0 +1,15 @@
+"use strict";
+//  Variable
+// declare a variable 
+
+var firstName = "Parth"
+
+// use a variable
+console.log(firstName)
+
+// Change value 
+
+firstName = "Abhishek Rai"
+console.log(firstName)
+
+
