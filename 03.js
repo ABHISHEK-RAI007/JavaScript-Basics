76275535b41f2608c128c46cc85184b1d2695bb2
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-05-31 18:07:42 +0530

    Add files via upload
    My First JavaScript learning files

---
+++ 03.js
@@ -0,0 +1,31 @@
+// Rules for naming variables
+// you can not start with number 
+// example :-
+// 1value (invalid)
+// value1 (valid)
+"use strict";
+
+var value1 = 4;
+console.log(value1 ** 0.5);
+
+// you can use only underscore _ or dollar symbol
+// first_name(valid)
+// _firstname(valid)
+
+var _firstname = "Pooja Sangwan";
+console.log(_firstname );
+
+// firt$name (valid)
+// $firstname (valid)
+
+//  you can not use other Symbol
+
+// you cannot use spaces 
+var first_name = "Pooja"; // snake case writing
+var firstname = "Pooja"; // camel case writing
+
+// firs name (invalid)
+
+
+///convention
+// start with small letter with camlecase
