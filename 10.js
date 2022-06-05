e23700fce369a01c9bef48aef48972749779b424
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-05 23:01:36 +0530

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,9 @@
+// template string 
+let age = 22;
+let firstName = "Harshit"
+ 
+// Print of  "my name is harshit and my age is 22 "
+// let aboutMe = "my name is " + firstName + " and my age is " + age; 
+
+let aboutMe = `My name is ${firstName} and my age is ${age}`
+console.log(aboutMe)
