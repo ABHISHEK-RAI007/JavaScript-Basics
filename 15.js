b72c854c87ed76cfccf37427291c752fcffbf913
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-07 23:26:33 +0530

    Add files via upload

---
+++ 15.js
@@ -0,0 +1,17 @@
+// ternary operator 
+
+let age = 4;
+let drink ;
+ if(age>=5){
+     drink = "Coffee"
+ }else{
+     drink = "Milk"
+ }
+ console.log(drink);
+
+// ternary operator / conditional operator
+  
+age = 8;
+drink = age>=5 ? "coffee" : "milk";
+console.log(drink);
+
