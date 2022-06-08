9263768d7c649de38556eb43029d61903c9d0587
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-08 10:34:52 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,15 @@
+"use strict";
+//  Variable
+// declare a variable 
+
+var firstName = "Pooja Sangwan"
+
+// use a variable
+console.log(firstName)
+
+// Change value 
+
+firstName = "Abhishek Rai"
+console.log(firstName)
+
+
