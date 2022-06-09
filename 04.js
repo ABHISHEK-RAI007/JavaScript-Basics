99ab0840d7475b356fc298ad7f89fa6154a02040
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-09 10:09:30 +0530

    Add files via upload

---
+++ 04.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// let keyword
+// declare variable with let keyword
+
+// var variable = "Pooja";
+let firstname = "Pooja";
+let firstname = "Abhishek";
+console.log(firstname);
+
+// block scope vs function scope (covered later in this video)
