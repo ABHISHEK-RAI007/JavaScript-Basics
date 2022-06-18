777932070163bedd09108fb594957f0dcdd1fd20
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-18 12:35:12 +0530

    Add files via upload

---
+++ 09.js
@@ -0,0 +1,24 @@
+// string concatenation 
+
+let string1 = "Abhishek";
+let string2 = "Rai";
+
+let fullName = string1 + " " + string2;
+console.log(fullName);
+
+string1 = "17";
+string2 = "10";
+let newString = string1 + string2;
+console.log(newString);
+console.log(typeof newString)
+
+ newString = +string1 + +string2;
+ console.log(newString);
+ console.log(typeof newString)
+
+// Answer
+// Abhishek Rai
+// 09.js:12 1710
+// 09.js:13 string
+// 09.js:16 27
+// 09.js:17 number
